1e5dbe6aab465a5dfd1bc354c659976d187f09d5
deadcheat <deadcheat@users.noreply.github.com>
2018-03-09 02:18:46 +0900

    added static values

--- lib/addreport.dart
+++ lib/addreport.dart
@@ -7,12 +7,19 @@ class AddReportDialog extends StatefulWidget {
 
 class AddReportDialogState extends State<AddReportDialog> {
   final formKey = new GlobalKey<FormState>();
-  double _first_diastolic = 135.0;
-  double _first_systolic = 85.0;
-  double _first_pulse = 80.0;
-  double _second_diastolic = 135.0;
-  double _second_systolic = 85.0;
-  double _second_pulse = 80.0;
+  static final double _diastolic_default = 130.0;
+  static final double _systolic_default = 80.0;
+  static final double _pulse_default = 80.0;
+  static final double _bp_max = 220.0;
+  static final double _bp_min = 30.0;
+  static final double _p_max = 150.0;
+  static final double _p_min = 30.0;
+  double _first_diastolic = _diastolic_default;
+  double _first_systolic = _systolic_default;
+  double _first_pulse = _pulse_default;
+  double _second_diastolic = _diastolic_default;
+  double _second_systolic = _systolic_default;
+  double _second_pulse = _pulse_default;
   int _timing = 0;
   DateTime _date = new DateTime.now();
 
@@ -89,9 +96,9 @@ class AddReportDialogState extends State<AddReportDialog> {
               new Slider(
                   value: _first_diastolic,
                   activeColor: Colors.green,
-                  min: 30.0,
-                  max: 250.0,
-                  divisions: 220,
+                  min: _bp_min,
+                  max: _bp_max,
+                  divisions: (_bp_max - _bp_min).toInt(),
                   onChanged: (double value) {
                     setState(() {
                       _first_diastolic = value;
@@ -107,9 +114,9 @@ class AddReportDialogState extends State<AddReportDialog> {
               new Slider(
                   value: _first_systolic,
                   activeColor: Colors.green,
-                  min: 30.0,
-                  max: 250.0,
-                  divisions: 220,
+                  min: _bp_min,
+                  max: _bp_max,
+                  divisions: (_bp_max - _bp_min).toInt(),
                   onChanged: (double value) {
                     setState(() {
                       _first_systolic = value;
@@ -124,9 +131,9 @@ class AddReportDialogState extends State<AddReportDialog> {
               new Slider(
                   value: _first_pulse,
                   activeColor: Colors.green,
-                  min: 30.0,
-                  max: 150.0,
-                  divisions: 120,
+                  min: _p_min,
+                  max: _p_max,
+                  divisions: (_p_max - _p_min).toInt(),
                   onChanged: (double value) {
                     setState(() {
                       _first_pulse = value;
@@ -148,9 +155,9 @@ class AddReportDialogState extends State<AddReportDialog> {
               new Slider(
                   value: _second_diastolic,
                   activeColor: Colors.purple,
-                  min: 30.0,
-                  max: 250.0,
-                  divisions: 220,
+                  min: _bp_min,
+                  max: _bp_max,
+                  divisions: (_bp_max - _bp_min).toInt(),
                   onChanged: (double value) {
                     setState(() {
                       _second_diastolic = value;
@@ -166,9 +173,9 @@ class AddReportDialogState extends State<AddReportDialog> {
               new Slider(
                   value: _second_systolic,
                   activeColor: Colors.purple,
-                  min: 30.0,
-                  max: 250.0,
-                  divisions: 220,
+                  min: _bp_min,
+                  max: _bp_max,
+                  divisions: (_bp_max - _bp_min).toInt(),
                   onChanged: (double value) {
                     setState(() {
                       _second_systolic = value;
@@ -183,8 +190,8 @@ class AddReportDialogState extends State<AddReportDialog> {
               new Slider(
                   value: _second_pulse,
                   activeColor: Colors.purple,
-                  min: 30.0,
-                  max: 150.0,
+                  min: _p_min,
+                  max: _p_max,
                   divisions: 120,
                   onChanged: (double value) {
                     setState(() {
