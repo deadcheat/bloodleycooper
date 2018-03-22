e4332d5b7e322ed8fc239af7924f5fe9f23136ea
deadcheat <deadcheat@users.noreply.github.com>
2018-03-23 02:01:55 +0900

    added list selector

--- lib/database.dart
+++ lib/database.dart
@@ -7,8 +7,12 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:bloodpit/report.dart';
 
+Database db;
+
+Future close() async => db.close();
+
 Future<Report> addReport(Report report) async {
-  Database db = await getDB();
+  db = await getDB();
   final int resId = await db.insert("Reports", report.toMap());
   if (resId == 0) {
     return null;
@@ -17,15 +21,22 @@ Future<Report> addReport(Report report) async {
       resId, report.day, report.timing, report.first, report.second);
 }
 
+Future<Map<int, List<Report>>> findReports(int year, int month) async {
+  db = await getDB();
+  List<Map> reports =
+      await db.query("Reports", where: "$year = ? and $year = ?");
+  return Report.fromMap(reports);
+}
+
 Future<Database> getDB() async {
   Directory documentsDirectory = await getApplicationDocumentsDirectory();
   String path = join(documentsDirectory.path, "assets_bloodpit.db");
 
-  Database database = await openDatabase(path, version: 1,
+  db = await openDatabase(path, version: 1,
       onCreate: (Database db, int version) async {
     // When creating the db, create the table
     await db.execute(
-        "CREATE TABLE Reports (id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER, timing INTEGER, first_min INTEGER, first_max INTEGER, first_pul INTEGER, second_min INTEGER, second_max INTEGER, second_pul INTEGER)");
+        "CREATE TABLE Reports (id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER, year INTEGER, month INTEGER, day INTEGER, timing INTEGER, first_min INTEGER, first_max INTEGER, first_pul INTEGER, second_min INTEGER, second_max INTEGER, second_pul INTEGER)");
   });
-  return database;
+  return db;
 }
--- lib/report.dart
+++ lib/report.dart
@@ -21,9 +21,17 @@ class Report {
         ((this.first.pulse + this.second.pulse) / 2).round());
   }
 
+  int mapKey() {
+    DateTime d = this.day;
+    return (d.year * 1000) + (d.month * 10) + (d.day);
+  }
+
   Map toMap() {
     Map map = {
       "date": day.millisecondsSinceEpoch,
+      "year": day.year,
+      "month": day.month,
+      "day": day.day,
       "timing": timing.toCode(),
       "first_min": first.minimal,
       "first_max": first.maximal,
@@ -36,6 +44,36 @@ class Report {
       map["id"] = id;
     }
     return map;
+  }
+
+  static Map<int, List<Report>> fromMap(List<Map> data) {
+    Map<int, List<Report>> result = new Map<int, List<Report>>();
+    data.forEach((value) {
+      Report r = new Report(
+        value["id"],
+        value["day"],
+        value["timing"],
+        new Measurement(
+          value["first_max"],
+          value["first_min"],
+          value["first_pul"],
+        ),
+        new Measurement(
+          value["second_max"],
+          value["second_min"],
+          value["second_pul"],
+        ),
+      );
+      int key = r.mapKey();
+      if (result.containsKey(key)) {
+        result[key].add(r);
+      } else {
+        List<Report> list = new List<Report>();
+        list.add(r);
+        result[key] = list;
+      }
+    });
+    return result;
   }
 }
 
