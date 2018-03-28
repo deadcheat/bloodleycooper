8a3c9d43c5db79f9de6f058fe5d9f63bc4aa4f23
deadcheat <deadcheat@users.noreply.github.com>
2018-03-29 00:11:28 +0900

    update version

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:meta/meta.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 
 import 'package:bloodpit/addreport.dart';
 import 'package:bloodpit/report.dart';
